8e0dd75a798d310d35798b06f169071559057561
Jeyananthan Sivakumaran and Lauren Marcus <lauren.e.marcus@gmail.com>
2015-02-19 12:58:21 -0800

    Link lists and tasks together.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,70 @@
+var List = {
+  list: "",
+  tasks: [],
+}
+
 $(document).ready(function() {
-  $("form#form").submit(function(event) {
+  var currentList = Object.create(List);
+
+  var displayTasks = function(){
+    $("ul.tasks").text("");
+    if(currentList.tasks.length>0){
+      currentList.tasks.forEach(function(task) {
+        $("ul.tasks").append("<li><span class='task'>" + task.task + "</span></li>");
+      });
+    }
+  }
+  $("form#form-list").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedList = $("input#list").val();
+
+    var newList = { list: inputtedList, tasks: [] };
+
+    $("ul#lists").append("<li><span class='list'>" + newList.list + "</span></li>");
+
+    $("input#list").val("");
+
+    $('.list').last().click(function() {
+
+      // $("")
+
+      displayTasks();
+
+      $(".tasksdisplay h2").text(newList.list);
+
+      currentList = newList;
+
+
+    });
+  });
+
+
+
+
+
+  $("form#form-task").submit(function(event) {
     event.preventDefault();
 
     var inputtedTask = $("input#task").val();
     var newTask = { task: inputtedTask };
 
-    $("ul.tasks").append("<li><span class='task'>" + newTask.task + "</span></li>");
+    currentList.tasks.push(newTask);
 
-    $("input#task").val("");
+
+displayTasks();
+    // $("ul.tasks").text("");
+    // currentList.tasks.forEach(function(task) {
+    //   $("ul.tasks").append("<li><span class='task'>" + task.task + "</span></li>");
+    // });
+
+// $("ul.tasks").append("<li><span class='task'>" + newTask.task + "</span></li>");
 
     $(".tasks li").last().click(function() {
       $(this).css('text-decoration', 'line-through');
     });
+
+    $("input#task").val("");
+
   });
 });
